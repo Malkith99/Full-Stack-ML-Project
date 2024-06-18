9b718e982f60f426e6f08d457f010b1a7052fc98
Malkith99 <malkithamanda@gmail.com>
2024-06-18 17:42:19 +0530

    app.py is updated

--- Website/app.py
+++ Website/app.py
@@ -1,9 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app=Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=['POST','GET'])
 def index():
+    if request.method=='POST':
+        gender=request.form['gender']
+        married=request.form['married']
+        dependents=request.form['dependents']
+        education=request.form['education']
+        selfEmployed=request.form['selfEmployed']
+        coapplicantIncome=request.form['coapplicantIncome']
+        loanAmount=request.form['loanAmount']
+        loanAmountTerm=request.form['loanAmountTerm']
+        if request.form.getlist('creditHistory'):
+            creditHistory=1
+        else:
+            creditHistory=0
+        print(creditHistory)
+
+
     return render_template('index.html')   
 
 if __name__=="__main__":
