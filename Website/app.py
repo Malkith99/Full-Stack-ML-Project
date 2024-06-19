03b3779f84237b81882c58e6a51dffdac8fc78e8
Malkith99 <malkithamanda@gmail.com>
2024-06-19 16:57:02 +0530

    update the file by model calling in the app.py

--- Website/app.py
+++ Website/app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template,request
+import pickle
+import numpy as np
 
 app=Flask(__name__)
+
+
+def prediction(lst):
+    filename = 'model/predictor.pickle'
+    with open(filename, 'rb') as file:
+        model = pickle.load(file)
+    pred_value = model.predict([lst])
+    return pred_value
 
 @app.route('/',methods=['POST','GET'])
 def index():
@@ -17,8 +27,14 @@ def index():
             creditHistory=1
         else:
             creditHistory=0
-        print(creditHistory)
-
+        #print(creditHistory)
+        feature_list=[gender,married,dependents,education,selfEmployed,coapplicantIncome,loanAmount,loanAmountTerm,creditHistory]
+        
+        pred_value=prediction(feature_list)
+        if pred_value[0]==1:
+            return render_template('index.html',pred="Loan Approved")
+        else:
+            return render_template('index.html',pred="Loan Rejected")
 
     return render_template('index.html')   
 
