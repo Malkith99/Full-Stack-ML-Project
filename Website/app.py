c8e1e3940590d6baf966cad6843d47e58ca2a6b0
Malkith99 <malkithamanda@gmail.com>
2024-06-19 18:54:27 +0530

    First Stage of application file is completed

--- Website/app.py
+++ Website/app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template,request
 import pickle
-import numpy as np
 
 app=Flask(__name__)
 
 
 def prediction(lst):
-    filename = 'model/predictor.pickle'
-    with open(filename, 'rb') as file:
+    filename = 'model/predictor1.pickle'
+    with open(filename, 'rb') as file:      #rb- read binart
         model = pickle.load(file)
     pred_value = model.predict([lst])
     return pred_value
@@ -20,23 +19,38 @@ def index():
         dependents=request.form['dependents']
         education=request.form['education']
         selfEmployed=request.form['selfEmployed']
+        applicantIncome=request.form['applicantIncome']
         coapplicantIncome=request.form['coapplicantIncome']
         loanAmount=request.form['loanAmount']
         loanAmountTerm=request.form['loanAmountTerm']
+        propertyArea=request.form['propertyArea']
         if request.form.getlist('creditHistory'):
             creditHistory=1
         else:
             creditHistory=0
-        #print(creditHistory)
-        feature_list=[gender,married,dependents,education,selfEmployed,coapplicantIncome,loanAmount,loanAmountTerm,creditHistory]
-        
-        pred_value=prediction(feature_list)
-        if pred_value[0]==1:
-            return render_template('index.html',pred="Loan Approved")
-        else:
-            return render_template('index.html',pred="Loan Rejected")
 
-    return render_template('index.html')   
+
+        feature_list=[int(gender),int(married),int(dependents),int(education),int(selfEmployed),int(applicantIncome),int(coapplicantIncome),int(loanAmount),int(loanAmountTerm)]
+
+        property_list=['Semiurban','Urban','Rural']    
+        def encodingList(list,value):    
+            for item in list:
+                if item==value:
+                    feature_list.append(1)
+                else:
+                    feature_list.append(0)
+        encodingList(property_list,propertyArea)
+        feature_list.append(creditHistory)
+
+        print(feature_list)
+        pred_value=prediction(feature_list)
+        print(pred_value)
+        if pred_value[0]==1:
+            pred="Loan Approved"
+        else:
+            pred="Loan Rejected"
+        print(pred)
+    return render_template('index.html',pred=pred)   
 
 if __name__=="__main__":
     app.run(debug=True)
