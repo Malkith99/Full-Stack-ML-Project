c2f156220adbd6482411db792f608513429dae75
Malkith99 <malkithamanda@gmail.com>
2024-07-15 16:05:21 +0530

    add routes to the created pages

--- Website/client/src/App.js
+++ Website/client/src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header/Header.js';
-
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
+import Header from './components/Header.js';
+import Home from './Pages/Home.js';
+import About from './Pages/About.js';
 function App() {
   return (
-
-      <Header/>
+      <Router>
+        <>
+        <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/about" element={<About/>}></Route>
+          </Routes>
+        </>
+        </Router> 
   );
 }
 
