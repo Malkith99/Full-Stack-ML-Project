79dcbec1646cae53feee1c8b674d6621562c71ed
Malkith99 <malkithamanda@gmail.com>
2024-07-15 16:04:10 +0530

    create the draft about page

---
+++ Website/client/src/Pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../components/Header'
+import './about.css'
+
+export default function About() {
+    return (
+        <div>
+            <div>
+                <Header />
+            </div>
+            <div className="About">
+                <p>This is about page</p>
+            </div>
+        </div>
+    )
+}
